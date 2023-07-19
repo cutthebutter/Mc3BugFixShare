e972db824758e1be2f46c580a4f65db04ae5d21c
sozohoy <sozohoy@gmail.com>
2023-07-19 19:42:57 +0900

    feat: LogList 정의, 카테고리 enum으로 관리

--- DetectiveLog/DetectiveLog/Main/LogList.swift
+++ DetectiveLog/DetectiveLog/Main/LogList.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import CloudKit
+
+struct LogList {
+    let id: CKRecord.ID
+    let category: LogCategory
+    let title: String
+    let latestMemo: [String]
+    let isLocked: Bool
+    let isPinned: Bool
+    let createdAt: Date
+    let updatedAt: Date
+}
+
+enum LogCategory: Int {
+    case inProgress = 0
+    case complete = 1
+    case incomplete = 2
+}
