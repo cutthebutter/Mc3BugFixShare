4dd7b3226d567b9b944505c53e7b3ffba3d1fb4f
sozohoy <sozohoy@gmail.com>
2023-07-21 00:47:13 +0900

    feat: 카테고리에 따른 리스트 출력 구분, toolBar 기능 구현(#5)

--- DetectiveLog/DetectiveLog/Main/LogView.swift
+++ DetectiveLog/DetectiveLog/Main/LogView.swift
@@ -8,25 +8,54 @@
 import SwiftUI
 
 struct MainView: View {
-    @State var selection = "진행 중"
+    
+    @Environment(\.editMode) private var editMode
+    @State var isPresented = false
+    @State var isEditing = false
+    @State var selection = 0
     @State var multiSelection = Set<UUID>()
     var category = ["진행 중", "완결", "미완결"]
     
     var tempLog: [TempLog] = [
-        TempLog(id: UUID(), title: "질곡동 사건", createdAt: "7.17", updatedAt: "7.29", latestMemo: ["자라 한마리에 가격이 수백만원. 주변의 가게도 모두", "남자한테 좋다."], isPinned: true),
-        TempLog(id: UUID(), title: "자라 절도 사건", createdAt: "7.17", updatedAt: "7.17", latestMemo: ["방이 어지럽혀져 있고 문이 열려있다. 황금 파리가 꼬인 것으로 보아 사망한지 6일정도 지난듯 함"], isPinned: true),
-        TempLog(id: UUID(), title: "구소산 추락사건", createdAt: "7.17", updatedAt: "7.29", latestMemo: ["자라 한마리에 가격이 수백만원. 주변의 가게도 모두", "남자한테 좋다."], isPinned: false),
-        TempLog(id: UUID(), title: "구소산 추락사건", createdAt: "7.17", updatedAt: "7.29", latestMemo: ["자라 한마리에 가격이 수백만원. 주변의 가게도 모두", "남자한테 좋다."], isPinned: false),
-        TempLog(id: UUID(), title: "정제명 장염사건", createdAt: "7.17", updatedAt: "7.29", latestMemo: ["자라 한마리에 가격이 수백만원. 주변의 가게도 모두", "남자한테 좋다."], isPinned: false)
+        TempLog(id: UUID(), title: "질곡동 사건", createdAt: "7.17", updatedAt: "7.29", latestMemo: ["자라 한마리에 가격이 수백만원. 주변의 가게도 모두", "남자한테 좋다."], isPinned: true, category: LogCategory(rawValue: 0)!),
+        TempLog(id: UUID(), title: "자라 절도 사건", createdAt: "7.17", updatedAt: "7.17", latestMemo: ["방이 어지럽혀져 있고 문이 열려있다. 황금 파리가 꼬인 것으로 보아 사망한지 6일정도 지난듯 함"], isPinned: true, category: LogCategory(rawValue: 1)!),
+        TempLog(id: UUID(), title: "구소산 추락사건", createdAt: "7.17", updatedAt: "7.29", latestMemo: ["자라 한마리에 가격이 수백만원. 주변의 가게도 모두", "남자한테 좋다."], isPinned: false, category: LogCategory(rawValue: 1)!),
+        TempLog(id: UUID(), title: "구소산 추락사건", createdAt: "7.17", updatedAt: "7.29", latestMemo: ["자라 한마리에 가격이 수백만원. 주변의 가게도 모두", "남자한테 좋다."], isPinned: false, category: LogCategory(rawValue: 2)!),
+        TempLog(id: UUID(), title: "정제명 장염사건", createdAt: "7.17", updatedAt: "7.29", latestMemo: ["부산 갔다와서 배아프다고 자꾸 찡찡댐", "노란 장화를 사려고 함"], isPinned: false, category: LogCategory(rawValue: 0)!)
     ]
     
     var body: some View {
         NavigationView {
             VStack {
                 categoryPickerView
-                logList
+                    .onChange(of: selection) { _ in
+                        multiSelection = []
+                    }
+                switch selection {
+                case 0:
+                    inProgressLogList
+                case 1:
+                    completeLogList
+                case 2:
+                    incompleteLogList
+                default:
+                    Text("Error Occured")
+                }
+                Spacer()
             }
+
             .navigationTitle("사건 일지")
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    if !isEditing {
+                        bottomToolbarItem
+                    } else {
+                        bottomToolbarItemIsEditing
+                    }
+                    
+                }
+            }
+            .environment(\.editMode, .constant(self.isEditing ? EditMode.active : EditMode.inactive)).animation(Animation.linear(duration: 0.2), value: isEditing)
             .toolbar {
                 ToolbarItemGroup(placement: .navigationBarTrailing) {
                     Button {
@@ -36,49 +65,98 @@ struct MainView: View {
                     }
                     // EditButton을 활용하여 특정 값의 변화를 체크해서 이동하는 로직을 만들어야 할듯!
                     Menu {
-                        EditButton()
+                        Button {
+                            self.isEditing.toggle()
+                            if isEditing {
+                                    
+                            }
+                        } label: {
+                            Text(isEditing ? "Done" : "Edit")
+                        }
+
                     } label: {
                         Image(systemName: "ellipsis")
                     }
                 }
             }
         }
-        .toolbar {
-            ToolbarItemGroup(placement: .bottomBar) {
-                Button("") {
-                    print("IIII")
-                }
-                Button {
-                    print("글쓰기")
-                } label: {
-                    Image(systemName: "square.and.pencil")
-                        .foregroundColor(.black)
-                }
-            }
-        }
     }
     
     var categoryPickerView: some View {
-        Picker("Hi", selection: $selection) {
-            ForEach(category, id: \.self) {
-                Text($0)
+        Picker("Picker", selection: $selection) {
+            ForEach(category.indices, id: \.self) { index in
+                Text(category[index])
             }
         }
         .pickerStyle(.segmented)
         .padding()
     }
     
-    var logList: some View {
-        // category 0, 1, 2에 맞는 무언가가 필요해~
+    var inProgressLogList: some View{
         List(selection: $multiSelection) {
             ForEach(tempLog) { log in
-                LogCell(tempLog: log)
-                    .listRowInsets(EdgeInsets())
+                if log.category == .inProgress {
+                    LogCell(tempLog: log)
+                        .listRowInsets(EdgeInsets())
+                }
             }
         }
         .listStyle(.inset)
     }
     
+    var completeLogList: some View {
+        List(selection: $multiSelection) {
+            ForEach(tempLog) { log in
+                if log.category == .complete {
+                    LogCell(tempLog: log)
+                        .listRowInsets(EdgeInsets())
+                }
+            }
+        }
+        .listStyle(.inset)
+    }
+    
+    var incompleteLogList: some View {
+        List(selection: $multiSelection) {
+            ForEach(tempLog) { log in
+                if log.category == .incomplete {
+                    LogCell(tempLog: log)
+                        .listRowInsets(EdgeInsets())
+                }
+            }
+        }
+        .listStyle(.inset)
+    }
+    
+    var bottomToolbarItem: some View {
+        Group {
+            Button("") {
+                print("IIII")
+            }
+            Button {
+                print("글쓰기")
+            } label: {
+                Image(systemName: "square.and.pencil")
+                    .foregroundColor(.black)
+            }
+        }
+    }
+    
+    var bottomToolbarItemIsEditing: some View {
+        Group {
+            Button("이동") {
+                for selection in multiSelection {
+                    print("@Log - \(selection)")
+                    print("@Log - \(tempLog[tempLog.firstIndex(where: { $0.id == selection })!].title)")
+                }
+            }
+            Button {
+                print("글쓰기")
+            } label: {
+                Text("삭제")
+            }
+        }
+    }
     
 }
 
