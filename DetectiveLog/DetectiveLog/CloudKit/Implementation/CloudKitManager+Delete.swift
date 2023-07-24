35720df6e85fbd7dcbb277dd1d170199cca98327
sozohoy <sozohoy@gmail.com>
2023-07-25 03:48:24 +0900

    refactor: 파일 분리(#14)

--- DetectiveLog/DetectiveLog/CloudKit/Implementation/CloudKitManager+Delete.swift
+++ DetectiveLog/DetectiveLog/CloudKit/Implementation/CloudKitManager+Delete.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import CloudKit
+
+extension CloudKitManager {
+    /// func deleteLogRecord: CloudKit Database에서 Log Record를 삭제합니다.
+    /// - Parameter: Log
+    func deleteLogRecord(log: Log) {
+        guard let recordId = log.id else { return }
+        container.delete(withRecordID: recordId) { recordId, error in
+            if let error = error {
+                print("@Log deleteLogRecord Error - \(error.localizedDescription)")
+            }
+            print("@Log - \(log.title) 삭제 완료!")
+        }
+    }
+}
