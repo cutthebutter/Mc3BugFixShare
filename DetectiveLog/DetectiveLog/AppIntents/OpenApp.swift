11adcccd3d3def40ce42ccf22323b58ac937197e
cutthebutter (Shannon) <uhoo1216@gmail.com>
2023-07-25 12:01:11 +0900

    feat/앱 intent 작업 중(#15)

---
+++ DetectiveLog/DetectiveLog/AppIntents/OpenApp.swift
@@ -0,0 +1,34 @@
+//
+//  OpenApp.swift
+//  DetectiveLog
+//
+//  Created by semini on 2023/07/25.
+//
+
+import Foundation
+import AppIntents
+import SwiftUI
+
+//TODO: iOS 15에서도 구동하는 방법을 찾을 것,,
+@available(iOS 16, *)
+struct OpenApp : AppIntent {
+    init() {
+    }
+
+    //밖에 보이는 타이틀
+    static var title : LocalizedStringResource = "앱 바로가기"
+    
+    static var description: IntentDescription = IntentDescription("앱을 바로 실행할 수 있는 기능입니다. ")
+    
+    static var openAppWhenRun = true
+    //내부에 보이는 작동 타이틀
+    static var parameterSummary: some ParameterSummary {
+        Summary("앱 열기")
+    }
+    
+    @MainActor
+    func perform() async throws -> some IntentResult {
+        LogViewModel.shared.navigateToMain()
+        return .result()
+    }
+}
