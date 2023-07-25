b8c9955e82f82134e6736d6c15d43a5eedda54ac
sozohoy <sozohoy@gmail.com>
2023-07-26 04:51:50 +0900

    feat: 프로퍼티 옵셔널 처리, 배열 형태의 데이터 리턴하는 메소드 구현(#22)

--- DetectiveLog/DetectiveLog/Main/Model/LogMemo.swift
+++ DetectiveLog/DetectiveLog/Main/Model/LogMemo.swift
@@ -8,10 +8,19 @@
 import Foundation
 import CloudKit
 
-struct LogMemo: Identifiable {
-    let id: CKRecord.ID
+struct LogMemo: Identifiable, Hashable {
+    
+    let id: CKRecord.ID?
     let referenceId: CKRecord.Reference?
     var memo: String
     let logMemoDate: Date
     var createdAt: Date
+    
+    static func fetchLogMemoRecord(log: Log, _ completion: @escaping (([LogMemo]) -> ())) {
+        let cloudKitManager = CloudKitManager.shared
+        cloudKitManager.fetchLogMemoRecord(log: log) { logMemo in
+            completion(logMemo)
+        }
+    }
+    
 }
