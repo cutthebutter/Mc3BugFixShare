0bf2408816d986adda0eda7dc560b7e7e7dbc208
sozohoy <sozohoy@gmail.com>
2023-07-26 18:10:30 +0900

    add: DetailLogView(#22)

---
+++ DetectiveLog/DetectiveLog/TempDetail/DetailLogView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailLogView.swift
+//  DetectiveLog
+//
+//  Created by 한지석 on 2023/07/26.
+//
+
+import SwiftUI
+
+struct DetailLogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailLogView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailLogView()
+    }
+}
