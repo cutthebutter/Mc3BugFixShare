4100d7bad044ccbcc1d7d762583c5897091f48be
sozohoy <sozohoy@gmail.com>
2023-07-26 23:44:20 +0900

    feat: UUID 추가, 메소드 async/await로 수정(#22)

--- DetectiveLog/DetectiveLog/Main/Model/LogMemo.swift
+++ DetectiveLog/DetectiveLog/Main/Model/LogMemo.swift
@@ -10,17 +10,16 @@ import CloudKit
 
 struct LogMemo: Identifiable, Hashable {
     
-    let id: CKRecord.ID?
+    let id: UUID
+    let recordId: CKRecord.ID?
     let referenceId: CKRecord.Reference?
     var memo: String
     let logMemoDate: Date
     var createdAt: Date
-    
-    static func fetchLogMemoRecord(log: Log, _ completion: @escaping (([LogMemo]) -> ())) {
+
+    static func fetchLogMemoRecord(log: Log) async -> [LogMemo] {
         let cloudKitManager = CloudKitManager.shared
-        cloudKitManager.fetchLogMemoRecord(log: log) { logMemo in
-            completion(logMemo)
-        }
+        return await cloudKitManager.fetchLogMemoRecord(log: log)
     }
-    
+
 }
