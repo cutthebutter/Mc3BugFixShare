ca91d59a92f07a6099501ff54f9d14cfbbc285eb
sozohoy <sozohoy@gmail.com>
2023-07-30 03:48:54 +0900

    feat: 메모 잠금시 모자이크 기능 구현(#27)

--- DetectiveLog/DetectiveLog/Main/View/LogCell.swift
+++ DetectiveLog/DetectiveLog/Main/View/LogCell.swift
@@ -18,13 +18,11 @@ struct LogCell: View {
             
             //MARK: 제목
             HStack(spacing: 0) {
-                // AppleSDGothicNeoB
                 Text("")
                     .frame(width: 32, height: 0)
                 Text(log.title)
                     .font(.custom("AppleSDGothicNeo-Bold", size: 18))
                     .frame(height: 29)
-//                    .background(.black)
                     
                 Image("pin")
                     .opacity(log.isPinned == 1 ? 1 : 0)
@@ -32,7 +30,6 @@ struct LogCell: View {
                     
                 Spacer()
             }
-//            .padding(.leading, 32)
             .padding(.top, 12)
             
             //MARK: 최근 메모
@@ -45,6 +42,7 @@ struct LogCell: View {
                     }
                 }
             }
+            .redacted(reason: log.isLocked == 0 ? [] : .placeholder)
             .padding(.leading, 32)
             .padding(.top, 4)
             
@@ -55,28 +53,18 @@ struct LogCell: View {
                 .frame(height: 21)
                 .padding(.top, 2)
                 .padding(.leading, 52)
+                .redacted(reason: log.isLocked == 0 ? [] : .placeholder)
             
             Spacer()
         }
         .frame(height: 118)
         .frame(maxWidth: .infinity)
-//        .background(.indigo.opacity(0.1))
-        
+
     }
     
     func formatDateToString(date: Date) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "y.M.d"
         return dateFormatter.string(from: date)
-    }
-}
-
-struct LogCell_Previews: PreviewProvider {
-    static var previews: some View {
-        LogCell(log: Log(id: UUID(),
-                         recordId: nil,
-                         category: .inProgress,
-                         title: "슈프림 양념치킨",
-                         latestMemo: ["한지석", "코지"], isBookmarked: 0, isLocked: 0, isPinned: 1, createdAt: Date(), updatedAt: Date()))
     }
 }
